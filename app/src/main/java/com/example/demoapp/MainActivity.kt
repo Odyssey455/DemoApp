dec77a1cee86750ee4c736e637ee43c36139ab17
Adrian Fong <adrain2000@live.com>
2022-07-19 10:46:09 +0800

    Week 3 Push

--- app/src/main/java/com/example/demoapp/MainActivity.kt
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -1,22 +1,35 @@
 package com.example.demoapp
 
+import android.content.Context
+import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    //Declaring Variables to be Attached
     lateinit var diceImg: ImageView
     lateinit var numberText: TextView
+    lateinit var editPlayerName: EditText
+    lateinit var playerName:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Attach variables via ID
         numberText = findViewById(R.id.numberText)
         diceImg = findViewById(R.id.diceImg)
+        editPlayerName = findViewById(R.id.editPlayerName)
+        playerName = findViewById(R.id.playerName)
+
+        //Setting up On Click Listener
         val rollButton: Button = findViewById(R.id.rollButton)
         rollButton.setOnClickListener{ rollDice()}
+        val updateButton = findViewById<Button>(R.id.updatePlayerNameBtn)
+        updateButton.setOnClickListener{updatePlayerName(it)}
     }
 
     private fun rollDice(){
@@ -37,5 +50,13 @@ class MainActivity : AppCompatActivity() {
 
         Toast.makeText(this,randomNumb.toString(),
             Toast.LENGTH_SHORT).show()
+    }
+
+    private fun updatePlayerName(view: View){
+        playerName.text = editPlayerName.text
+
+        //Hide Keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
